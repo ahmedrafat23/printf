baccb8b823cef1cd44df83807c0525631a763040
ahmedrafat23 <ahmedrafatdawood@gmail.com>
2023-09-17 05:37:55 -0700

    a

--- print_functions1.c
+++ print_functions1.c
@@ -37,6 +37,35 @@ int print_unsigned(va_list types, char buffer[],
 			return (write_unsgnd(0, i, buffer, flags, width, precision, size));
 	}
 
+
+/* PRINT UNSIGNED NUMBER IN HEXADECIMAL */
+/**
+ * print_hexadecimal - prints an unsigned number in hexadecimal notation
+ * @types: lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags: Calculates active flags
+ * @width: get width
+ * @precision: precision specification
+ * @size: size specifier
+ * @Return: Number of chars printed
+ */
+int print_hexadecimal(va_list types, char buffer[],
+		int flags, int width, int precision, int size)
+{
+	return (print_hexa(types, "0123456789abcdef", buffer,
+				flags, 'x', width, precision, size));
+}
+
+/* PRINT UNSIGNED NUMBER IN UPPER HEXADECIMAL */
+/**
+ * print_hexa_upper - prints an unsigned number in upper hexadecimal notation
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags: Calculates active flags
+ * @width: get width
+ * @precision: precision specification
+ * @size: size specifier
+ * */
 	/* PRINT UNSIGNED NUMBER IN OCTAL */
  /**
   * print_octal - prints an unsigned number in octal notation
@@ -70,3 +99,29 @@ int print_unsigned(va_list types, char buffer[],
 			buffer[i--] = (num % 8) + '\0';
 			num /= 8;
 		}
+/* PRINT HEXX NU, IN LOWER OR UPER */
+		/**
+		 * print_hexa - prints a hexadecimal number in lower or upper
+		 * @types: lista of arguments
+		 * @mao_to: Array of values to map the number to
+		 * @buffer:Buffer array to handle print
+		 * @flags: Calculates active flags
+		 * @flag_ch: Calculates active flags
+		 * @width: grt width
+		 * @precision: precision specification
+		 * @size: Size specifier
+		 * @size: Size specification
+		 * Return: Number of chars
+		 */
+		int print_hexa(va_list types, char map_to[], char buffer[],
+				int flags, char flag_ch, int width, int precision, int size)
+		{
+			int i = BUFF_SIZE - 2;
+			insigned long int num = va_arg(types, unsigned long int);
+			unsigned long int init_num = num;
+
+			UNUSED(width);
+
+			num = convert_size_unsgnd(num, size);
+
+			if (num == 0)
